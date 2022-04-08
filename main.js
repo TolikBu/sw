da83a51074854e0a3751388fe6585a0221653b54
Tolikbu <tolikelement@gmail.com>
2022-04-08 16:44:54 +0500

    add func

--- main.js
+++ main.js
@@ -7,33 +7,37 @@ const height = document.querySelector('.hright');
 const width = document.querySelector('.width');
 const planets = document.querySelector('.planets');
 const eye = document.querySelector('.color-eye');
+const statusBlock = document.createElement('div');
 
 
 const enterNum = () => {
-  input.addEventListener('input', (item) => {
-    let num
-    num = item.target.value
+  input.addEventListener('change', (item) => {
+    let id = item.target.value;
+
+    btnSubmit.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      sendData(`https://swapi.dev/api/people/${id}/`);
+      id = '';
+      input.value = '';
+    })
   })
 }
 enterNum();
 
-
-const sendData = () => {
-  return fetch('https://swapi.dev/api/people/4/', {
+const sendData = (url) => {
+  return fetch(url, {
     headers: {
       'Content-Type': 'application/json',
     },
-  }).then((res) => res.json()).then((res) => console.log(res));
+  }).then(res => {
+    if (!res.ok) {
+      return res.json();
+    }
+
+    return res.json().then(error => {
+      console.log(error);
+    });
+  }).then(res => console.log(res))
+  
 };
-
-btnSubmit.addEventListener('click', (e) => {
-  e.preventDefault();
-
-  sendData();
-
-  input.value = ' ';
-})
-
-
-
-
