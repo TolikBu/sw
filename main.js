609aba846e15afac31df62f0f436c51f55a1cfb9
Tolikbu <tolikelement@gmail.com>
2022-04-12 12:32:17 +0500

    fix adaptiv

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 'use strict'
 
-const input = document.querySelector('.hero-form__input');
 const form = document.querySelector('.hero-form');
-const btnSubmit = document.querySelector('.hero-form__button')
+const input = form.querySelector('.hero-form__input');
+const btnSubmit = form.querySelector('.hero-form__button')
 const statusBlock = document.querySelector('.hero-error');
+const planets = document.querySelector('.planets')
+const name = document.querySelector('.hero-card__name');
+const gender = document.querySelector('.gender');
+const height = document.querySelector('.height');
+const mass = document.querySelector('.width');
+const colorEye = document.querySelector('.color-eye');
+const textValid = 'Введите число от 1 до 83';
+const texError = 'Проблемы с сетью';
+const texErrorPeople = 'Данного персонажа не существует, попробуйте еще раз';
 
 
 
 // функция получение параметров персонажа
-const renderPeople = (url) => {
-  return fetch(url, {
+const renderPeople = async (url) => {
+  await fetch(url, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -17,66 +26,72 @@ const renderPeople = (url) => {
   }).then(res => {
     if (res.ok) {
       res.json().then((data) => {
-        document.querySelector('.hero-card__name').textContent = data.name;
-        document.querySelector('.gender').textContent = data.gender;
-        document.querySelector('.height').textContent = data.height;
-        document.querySelector('.width').textContent = data.mass;
-        document.querySelector('.color-eye').style.backgroundColor = data.eye_color;
+        name.textContent = data.name;
+        gender.textContent = data.gender;
+        height.textContent = data.height;
+        mass.textContent = data.mass;
+        colorEye.style.backgroundColor = data.eye_color;
       })
+    } else {
+      console.log(error);
     }
-  })
+  }).catch((error) => {
+    statusBlock.textContent = texErrorPeople;
+    statusBlock.style.display = 'block';
+    setTimeout(() => {
+      statusBlock.style.display = 'none';
+    }, 2000);
+  });
 };
 
 // функция получения планеты
-const renderPlanets = (url) => {
-  return fetch(url, {
+const renderPlanets = async (url) => {
+  await fetch(url, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
   }).then(res => {
     if (res.ok) {
+
       res.json().then((data) => {
-        document.querySelector('.planets').textContent = data.name;
+        planets.textContent = data.name;
       })
-    }
+    } 
+  }).catch((error) => {
+    console.log(error);
   })
 };
 
+
 // функция отрисовки полученных пааметров
 const requestData = () => {
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let id = input.value;
-    let isError = false;
 
+  if (!/[\d]/g.test(input.value) && input.value !== ' ') {
+
+    statusBlock.textContent = textValid;
+    statusBlock.style.display = 'block';
+    setTimeout(() => {
+      statusBlock.style.display = 'none';
+    }, 2000);
     
+  } else {
+    let idPeople = input.value;
+    renderPeople(`https://swapi.dev/api/people/${idPeople}/`)
 
-    if (!/[\d]/g.test(input.value) && input.value !== '') {
-      statusBlock.textContent = 'Введите число от 1 до 83';
-      statusBlock.style.display = 'block';
-      setTimeout(() => {
-        statusBlock.style.display = 'none';
-      }, 2000);
-    }
+    // let idPlanets = idPeople(parseInt(idPeople.data.homeworld.match(/d+/), 10))
 
-    if (!isError) {
-      renderPeople(`https://swapi.dev/api/people/${id}/`).catch((error) => {
-        statusBlock.textContent = 'Проблемы с сетью';
-        statusBlock.style.display = 'block';
-        setTimeout(() => {
-          statusBlock.style.display = 'none';
-        }, 2000);
-      });
+    renderPlanets(`https://swapi.dev/api/planets/${idPeople
+}/`)
+    
+  }
 
-      renderPlanets(`https://swapi.dev/api/planets/${id}/`).catch((error) => {
-        console.log(error);
-      });
-
-      input.value = '';
-    }
-
-  });
-
+  input.value = '';
+  
 }
-requestData();
+
+// функция отправки формы 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  requestData();
+});
