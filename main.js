90a01a37beccb5ed77e4b458c993220f3b6f9b8b
TolikBu <tolikelement@gmail.com>
2022-04-26 11:04:36 +0500

    final fix, all work

--- main.js
+++ main.js
@@ -1,108 +1,96 @@
-'use strict'
+'use strict';
 
 const form = document.querySelector('.hero-form');
 const input = form.querySelector('.hero-form__input');
-const btnSubmit = form.querySelector('.hero-form__button')
 const statusBlock = document.querySelector('.hero-error');
-const planets = document.querySelector('.planets')
+const planet = document.querySelector('.planets');
 const name = document.querySelector('.hero-card__name');
 const gender = document.querySelector('.gender');
 const height = document.querySelector('.height');
 const mass = document.querySelector('.width');
 const colorEye = document.querySelector('.color-eye');
-const textValid = 'Введите число от 1 до 83';
-const texError = 'Проблемы с сетью';
-const texErrorPeople = 'Данного персонажа не существует, попробуйте еще раз';
 
-
-
-
-// функция получение параметров персонажа
-const renderPeople = async (url) => {
-  await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then(res => {
-    if (res.ok) {
-      res.json().then((data) => {
-        name.textContent = data.name;
-        gender.textContent = data.gender;
-        height.textContent = data.height;
-        mass.textContent = data.mass;
-        colorEye.style.backgroundColor = data.eye_color;
-        home(data.homeworld);
-      })
-    } else {
-      statusBlock.textContent = texErrorPeople;
-      statusBlock.style.display = 'block';
-      setTimeout(() => {
-        statusBlock.style.display = 'none';
-      }, 2000);
-    }
-    
-  }).catch((error) => {
-    console.log(error);
-    statusBlock.textContent = texError;
-    statusBlock.style.display = 'block';
-    setTimeout(() => {
-      statusBlock.style.display = 'none';
-    }, 2000);
-  });
-
+//блок с текстом ошибки
+const showError = (text) => {
+  statusBlock.textContent = text;
+  statusBlock.style.display = 'block';
+  setTimeout(() => {
+    statusBlock.style.display = 'none';
+  }, 2000);
 };
 
-// получение url на планету
-const home = (home) => {
-  renderPlanets(home)
-}
+//валидация на число
+const checkValid = (str) => {
+  return !/[\d]/g.test(str) && str !== ' ';
+};
 
-// функция получения планеты
-const renderPlanets = (url) => {
-
+// функция получение параметров персонажа
+const getPeople = (url) => {
   return fetch(url, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
-  }).then(res => {
-    if (res.ok) {
-
-      res.json().then((data) => {
-        planets.textContent = data.name;
-      })
-    }
-  }).catch((error) => {
-    console.log(error);
   })
+    .then((res) => {
+      if (res.ok) return res.json();
+
+      if (res.status === 404) showError('Данного персонажа не существует, попробуйте еще раз');
+
+      showError('Не известная ошибка');
+    })
+    .catch((error) => {
+      showError('Ошибка:', error.message);
+    });
 };
 
+// функция получения планеты
+const getPlanet = (url) => {
+  return fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
+
+      if (res.status === 404) showError('Планета не найдена');
+
+      showError('Не известная ошибка');
+    })
+    .catch((error) => showError('Ошибка:', error.message));
+    
+};
 
 // функция отрисовки полученных пааметров
 const requestData = () => {
-  let idPeople = input.value;
-  
+  const idPeople = input.value;
 
-  if (!/[\d]/g.test(input.value) && input.value !== ' ') {
-
-    statusBlock.textContent = textValid;
-    statusBlock.style.display = 'block';
-    setTimeout(() => {
-      statusBlock.style.display = 'none';
-    }, 2000);
-
-  } else {
-
-    renderPeople(`https://swapi.dev/api/people/${idPeople}/`)
-
+  if (checkValid(input.value)) {
+    showError('Введите число');
+    return;
   }
 
-  input.value = '';
+  getPeople(`https://swapi.dev/api/people/${idPeople}/`)
+  .then((person) => {
+    getPlanet(person.homeworld).then((homeWorld) => {
+      renderPerson(person, homeWorld);
+    });
+  });
+};
 
+const renderPerson = (person, homeWorld) => {
+
+  name.textContent = person.name;
+  gender.textContent = person.gender;
+  height.textContent = person.height;
+  mass.textContent = person.mass;
+  colorEye.style.backgroundColor = person.eye_color;
+  planet.textContent = homeWorld.name;
 }
 
-// функция отправки формы 
+// функция отправки формы
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   requestData();
